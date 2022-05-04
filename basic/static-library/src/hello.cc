ce530772953e6fbb2c6f6d1dc0e50689a9284b9a
zll600 <3400692417@qq.com>
2022-05-04 20:05:39 +0800

    use static-library

---
+++ basic/static-library/src/hello.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "static/hello.h"
+
+void Hello::Print() {
+    std::cout << "Hello Headers!" << std::endl;
+}
---
+++ basic/static-library/src/main.cc
@@ -0,0 +1,7 @@
+#include "static/hello.h"
+
+int main(void) {
+    Hello h;
+    h.Print();
+    return 0;
+}
