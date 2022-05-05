111201c35040dd22972b8d5abd67c145048eb580
zll600 <3400692417@qq.com>
2022-05-05 09:58:14 +0800

    add configure-file usage

---
+++ code-generation/configure-files/main.cc
@@ -0,0 +1,10 @@
+#include "ver.h"
+#include "path.h"
+
+#include <iostream>
+
+int main(void) {
+    std::cout << "Hello Version " << ver << "!" << std::endl;
+    std::cout << "Path is " << path << std::endl;
+    return 0;
+}
